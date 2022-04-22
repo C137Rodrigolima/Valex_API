6610174864349e7d50c4d64f809c10a02584b1d6
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-04-22 11:06:32 -0300

    Preparando revisão de código

--- src/routers/paymentRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as paymentsController from "../controllers/paymentsController.js";
-
-const paymentRouter = Router();
-
-paymentRouter.post("/payment", paymentsController.paymentInPos);
-
-export default paymentRouter;
